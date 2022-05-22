49cf0c5f6c56036004d18d7a6f33df502ca72d43
Sanden <Fredrik.Sanden@wellspect.com>
2022-05-22 14:14:02 +0200

    Added image preview scaling

--- DynamicImageCropper.py
+++ DynamicImageCropper.py
@@ -6,7 +6,7 @@ background_colour = (15,16,18)
 #background_colour = (255,255,255)
 (width, height) = (1000, 1000)
 screen = py.display.set_mode((width, height))
-py.display.set_caption('Tutorial 1')
+py.display.set_caption('Image Cropper')
 screen.fill(background_colour)
 py.display.flip()
 
@@ -15,22 +15,26 @@ aspectX = 1
 aspectY = 1
 game_font = py.font.Font("Roboto-Regular.ttf",60)
 
-current_image_surface = py.image.load("img/1.jpg")
+current_image_surface = py.image.load("img/2.jpg")
 
 (imgWidth,imgHeight) = current_image_surface.get_size()
 
 if (imgWidth < imgHeight):
-  #Do something
-  helli =1
-else:
+  #imgHeight = max with
+  ratio = imgWidth/imgHeight
+  imgHeight = 800;
+  imgWidth = imgHeight*ratio
+else: 
+  ratio = imgHeight/imgWidth
+  imgWidth = 800;
+  imgHeight = imgWidth*ratio
 
-
-py.transform.scale(py.image.load("assets/img/frame-0.png").convert_alpha(), (96, 108))
+current_image_surface = py.transform.scale(py.image.load("img/2.jpg").convert_alpha(), (imgWidth, imgHeight))
 
 current_image_rect = current_image_surface.get_rect(center = (500,500))
 
 
-
+w, h = py.display.get_surface().get_size()
 running = True
 while running:
   screen.fill(background_colour)
@@ -53,10 +57,10 @@ while running:
 
 
   aspectX_surface = game_font.render(f"{int(aspectX)},",True,(0,255,220))
-  aspectX_rect = aspectX_surface.get_rect(center = (400,100))
+  aspectX_rect = aspectX_surface.get_rect(center = ((w-aspectX_surface.get_width())/2,100))
 
   aspectY_surface = game_font.render(f"{int(aspectY)}",True,(0,255,220))
-  aspectY_rect = aspectY_surface.get_rect(center = (460,100))
+  aspectY_rect = aspectY_surface.get_rect(center = ((w+aspectY_surface.get_width())/2,100))
 
  
   screen.blit(aspectX_surface,aspectX_rect)
